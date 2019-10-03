e181804856db1395d8093f3728367ce196f27ed2
Daniel Rodriguez <gameover_lifetime@outlook.com>
2019-10-03 16:57:05 -0500

    working converter

--- src/app/roman-numerals.service.spec.ts
+++ src/app/roman-numerals.service.spec.ts
@@ -1,49 +1,83 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing'
 
-import { RomanNumeralsService } from './roman-numerals.service';
-import { Component } from '@angular/core';
+import { RomanNumeralsService } from './roman-numerals.service'
+import { Component } from '@angular/core'
 
 describe('RomanNumeralsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [RomanNumeralsService]
-    });
-  });
+      providers: [
+        RomanNumeralsService
+      ]
+    })
+  })
 
-  it('should be created', inject([RomanNumeralsService], (service: RomanNumeralsService) => {
-    expect(service).toBeTruthy();
-  }));
+  it(
+    'should be created',
+    inject(
+      [
+        RomanNumeralsService
+      ],
+      (service: RomanNumeralsService) => {
+        expect(service).toBeTruthy()
+      }
+    )
+  )
 
-  it('should return roman numerals for 1-3 ', () => {
+  it('should return biggest unit', () => {
     const service: RomanNumeralsService = TestBed.get(RomanNumeralsService)
-    expect(service.arabicToRoman(1)).toEqual('I');
-    expect(service.arabicToRoman(2)).toEqual('II');
-    expect(service.arabicToRoman(3)).toEqual('III');
-  });
+    expect(service.unitAsigner('1', 1)).toEqual(1)
+    expect(service.unitAsigner('1', 2)).toEqual(10)
+    expect(service.unitAsigner('1', 3)).toEqual(100)
+    expect(service.unitAsigner('1', 4)).toEqual(1000)
+  })
 
-  it('should return "IV" on "4"', () => {
+  it('should return roman symbols for 1 <= values < 10', () => {
     const service: RomanNumeralsService = TestBed.get(RomanNumeralsService)
-    expect(service.arabicToRoman(4)).toEqual('IV');
-  });
+    expect(service.checkUnits(1)).toEqual('I')
+    expect(service.checkUnits(2)).toEqual('II')
+    expect(service.checkUnits(3)).toEqual('III')
+    expect(service.checkUnits(4)).toEqual('IV')
+    expect(service.checkUnits(5)).toEqual('V')
+    expect(service.checkUnits(6)).toEqual('VI')
+    expect(service.checkUnits(7)).toEqual('VII')
+    expect(service.checkUnits(8)).toEqual('VIII')
+    expect(service.checkUnits(9)).toEqual('IX')
+  })
 
-  it('should return roman numerals for 5-8', () => {
+  it('should return roman symbols for 10 <= values < 100', () => {
     const service: RomanNumeralsService = TestBed.get(RomanNumeralsService)
-    expect(service.arabicToRoman(5)).toEqual('V');
-    expect(service.arabicToRoman(6)).toEqual('VI');
-    expect(service.arabicToRoman(7)).toEqual('VII');
-    expect(service.arabicToRoman(8)).toEqual('VIII');
-  });
+    expect(service.checkTens(10)).toEqual('X')
+    expect(service.checkTens(20)).toEqual('XX')
+    expect(service.checkTens(30)).toEqual('XXX')
+    expect(service.checkTens(40)).toEqual('XL')
+    expect(service.checkTens(50)).toEqual('L')
+    expect(service.checkTens(60)).toEqual('LX')
+    expect(service.checkTens(70)).toEqual('LXX')
+    expect(service.checkTens(80)).toEqual('LXXX')
+    expect(service.checkTens(90)).toEqual('XC')
+  })
 
-  it('should return "IX" on "9"', () => {
+  it('should return roman symbols for 100 <= values <= 1000', () => {
     const service: RomanNumeralsService = TestBed.get(RomanNumeralsService)
-    expect(service.arabicToRoman(9)).toEqual('IX');
-  });
+    expect(service.checkHundreds(100)).toEqual('C')
+    expect(service.checkHundreds(200)).toEqual('CC')
+    expect(service.checkHundreds(300)).toEqual('CCC')
+    expect(service.checkHundreds(400)).toEqual('CD')
+    expect(service.checkHundreds(500)).toEqual('D')
+    expect(service.checkHundreds(600)).toEqual('DC')
+    expect(service.checkHundreds(700)).toEqual('DCC')
+    expect(service.checkHundreds(800)).toEqual('DCCC')
+    expect(service.checkHundreds(900)).toEqual('CM')
+    expect(service.checkHundreds(1000)).toEqual('M')
+  })
 
-  it('should return roman numerals for 10-13', () => {
+  it('should return any value from 1 to 1999 into its roman equivalent', () => {
     const service: RomanNumeralsService = TestBed.get(RomanNumeralsService)
-    expect(service.arabicToRoman(10)).toEqual('X');
-    expect(service.arabicToRoman(11)).toEqual('XI');
-    expect(service.arabicToRoman(12)).toEqual('XII');
-    expect(service.arabicToRoman(13)).toEqual('XIII');
-  });
-});
+    expect(service.arabicToRoman(1)).toEqual('I')
+    expect(service.arabicToRoman(499)).toEqual('CDXCIX')
+    expect(service.arabicToRoman(999)).toEqual('CMXCIX')
+    expect(service.arabicToRoman(1499)).toEqual('MCDXCIX')
+    expect(service.arabicToRoman(1999)).toEqual('MCMXCIX')
+  })
+})
--- src/app/roman-numerals.service.ts
+++ src/app/roman-numerals.service.ts
@@ -1,52 +1,177 @@
-import { Injectable } from '@angular/core';
-import { concat } from 'rxjs';
-import { DecimalPipe } from '@angular/common';
+import { Injectable } from '@angular/core'
+import { concat } from 'rxjs'
+import { DecimalPipe } from '@angular/common'
 
 @Injectable({
   providedIn: 'root'
 })
 export class RomanNumeralsService {
-  symbols = ['I','V','X','L','C','D','M']
+  symbols = [
+    'I', // 0
+    'V', // 1
+    'X', // 2
+    'L', // 3
+    'C', // 4
+    'D', // 5
+    'M' // 6
+  ]
 
-  constructor() { }
+  constructor () {}
 
-  arabicToRoman(arabic: number): String {
+  /*
+    unitAsigner
+      input:  Number
+      output: Number
+      Outputs leftmost side value with corresponding units
+  */
+
+  unitAsigner (num: string, units: number): number {
+    return parseInt(num) * Math.pow(10, units - 1)
+  }
+
+  /*
+    valueToRoman
+      input:  Number
+      output: String
+      Turns input value to roman symbols
+  */
+
+  valueToRoman (input: number): string {
+    let romaniced = ''
+
+    if (input < 10) {
+      romaniced = this.checkUnits(input)
+    } else if (input < 100) {
+      romaniced = this.checkTens(input)
+    } else if (input <= 1000) {
+      romaniced = this.checkHundreds(input)
+    }
+
+    return romaniced
+  }
+
+  /*
+    checkUnits
+      input:  Number
+      output: String
+      Turns input value < 10 to roman equivalent
+  */
+
+  checkUnits (input: number): string {
+    let romanicedUnits = ''
+    let overSymbol = this.symbols[2] //X
+    let mainSymbol = this.symbols[1] //V
+    let underSymbol = this.symbols[0] //I
+    if (input < 4) {
+      for (let index = 0; index < input; index++) {
+        romanicedUnits = romanicedUnits.concat(underSymbol)
+      }
+    } else if (input < 9) {
+      if (input == 4) {
+        romanicedUnits = underSymbol.concat(mainSymbol)
+      } else {
+        romanicedUnits = mainSymbol
+        for (let index = 0; index < input - 5; index++) {
+          romanicedUnits = romanicedUnits.concat(underSymbol)
+        }
+      }
+    } else if (input == 9) {
+      romanicedUnits = underSymbol.concat(overSymbol)
+    }
+
+    return romanicedUnits
+  }
+
+  /*
+    checkTens
+      input:  Number
+      output: String
+      Turns input value < 100 to roman equivalent
+  */
+
+  checkTens (input: number): string {
+    let romanicedTens = ''
+    let overSymbol = this.symbols[4] //C
+    let mainSymbol = this.symbols[3] //L
+    let underSymbol = this.symbols[2] //X
+
+    input = input / 10
+    if (input < 4) {
+      for (let index = 0; index < input; index++) {
+        romanicedTens = romanicedTens.concat(underSymbol)
+      }
+    } else if (input < 9) {
+      if (input == 4) {
+        romanicedTens = underSymbol.concat(mainSymbol)
+      } else {
+        romanicedTens = mainSymbol
+        for (let index = 0; index < input - 5; index++) {
+          romanicedTens = romanicedTens.concat(underSymbol)
+        }
+      }
+    } else if (input == 9) {
+      romanicedTens = underSymbol.concat(overSymbol)
+    }
+
+    return romanicedTens
+  }
+
+  /*
+    checkTens
+      input:  Number
+      output: String
+      Turns input value <= 1000 to roman equivalent
+  */
+
+  checkHundreds (input: number): string {
+    let romanicedHundreds = ''
+    let overSymbol = this.symbols[6] //M
+    let mainSymbol = this.symbols[5] //D
+    let underSymbol = this.symbols[4] //C
+
+    input = input / 100
+    if (input < 4) {
+      for (let index = 0; index < input; index++) {
+        romanicedHundreds = romanicedHundreds.concat(underSymbol)
+      }
+    } else if (input < 9) {
+      if (input == 4) {
+        romanicedHundreds = underSymbol.concat(mainSymbol)
+      } else {
+        romanicedHundreds = mainSymbol
+        for (let index = 0; index < input - 5; index++) {
+          romanicedHundreds = romanicedHundreds.concat(underSymbol)
+        }
+      }
+    } else if (input == 9) {
+      romanicedHundreds = underSymbol.concat(overSymbol)
+    } else if (input == 10) {
+      romanicedHundreds = overSymbol
+    }
+    return romanicedHundreds
+  }
+
+  /*
+    arabicToRoman
+      input:  Number
+      output: String
+      Turns any arabic number from 1 - 1999 to its roman equivalent
+  */
+
+  arabicToRoman (arabic: number): string {
     let roman = ''
-    let units = arabic.toString().length
+    let sArabic = arabic.toString().split('')
+    let fullLenght = sArabic.length
+    let units = sArabic.length
+    let nextUnit = sArabic.shift()
 
-    if (arabic <= 3) {
-      for (let index = 0; index < arabic; index++) {
-        roman = roman.concat(this.symbols[0])
-      }
-    } else if (arabic >= 4 && arabic <= 8){
-      if (arabic == 4) {
-        roman = 'I'.concat(this.symbols[1])
-      } else {
-        roman = this.symbols[1]
-        for (let index = 0; index < arabic-5; index++) {
-          roman = roman.concat(this.symbols[0])
-        }
-      }
-    } else if (arabic >= 9 && arabic <= 13) {
-      if (arabic == 9) {
-        roman = 'IX'
-      } else {
-        roman = 'X'
-        for (let index = 0; index < arabic-10; index++) {
-          roman = roman.concat('I')
-        }
-      }
+    for (let index = 0; index < fullLenght; index++) {
+      roman = roman.concat(this.valueToRoman(this.unitAsigner(nextUnit, units)))
+
+      units = sArabic.length
+      nextUnit = sArabic.shift()
     }
-    /*
-    if (arabic == 4) {
-      roman = 'IV'
-    } else {
-      for (let index = 0; index < arabic; index++) {
-        roman = roman.concat('I')
-      }
-    }
-    */
-    
-    return roman;
+
+    return roman
   }
 }
